5b25c4651b4996c43eeb5d8d53bb90bfde63c2e0
Marie-Ange106 <marieange3nguessong@gmail.com>
2023-04-20 10:40:41 +0100

    projet_ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.2eatFVM.app',
+  appName: '2eat',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
