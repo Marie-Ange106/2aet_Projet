3c3fbfcf8ace1ac464d1b16bd1d702898031e8ff
Marie-Ange106 <marieange3nguessong@gmail.com>
2023-04-23 13:56:30 +0100

    home page

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { NgbRatingConfig, NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent],
+  imports: [IonicModule, ExploreContainerComponent,NgbRatingModule,NgbTypeaheadModule],
+  providers: [NgbRatingConfig],
 })
 export class Tab1Page {
-  constructor() {}
+  constructor(config: NgbRatingConfig) {
+    config.max = 5;
+		config.readonly = true;
+  }
 }
